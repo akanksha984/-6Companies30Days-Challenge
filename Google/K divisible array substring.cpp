4a94155ad86b5fd8a5df05e0e4e19923f2f273a9
akanksha984 <97845909+akanksha984@users.noreply.github.com>
2023-01-25 18:17:50 +0530

    Create K divisible array substring.cpp

---
+++ Google/K divisible array substring.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int countDistinct(vector<int>& nums, int k, int p) 
+    {
+        unordered_set<string> s;
+        int n=nums.size();
+        
+        for(int i=0;i<n;i++)
+        {
+            int count=0;
+            string str="";
+            
+            for(int j=i;j<n;j++)
+            {
+                if(nums[j]%p==0)
+                    count++;
+                
+                if(count>k)
+                    break;
+                
+                str+=to_string(nums[j])+"->";
+                s.insert(str);
+            }
+        }
+        return s.size();
+    }
+};
